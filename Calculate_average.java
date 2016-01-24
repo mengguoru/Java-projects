b2c40507dc12da2bf4c477fd2c817927f1ac2b01
mengguoru <2506839494@qq.com>
2016-01-24 22:30:04 +0800

    modify Calculate_average.java

--- Calculate_average.java
+++ Calculate_average.java
@@ -1,15 +1,20 @@
-//input Lession'name and score,output average
-//but if it use Student Array maybe better(while name and score are attribute
+/*
+ * input   : Lession'name and score
+ * output  : average
+ *
+ * note    : but if it use Student Class Array maybe better(while name and score can be attribute
+*/
 import java.util.*;
 public class LessionAndScore{
 	public static void main(String[] args){
-		Scanner sc=new Scanner(System.in);
+		//initialize two array:Name Score
 		ArrayList<String>subjectName=new ArrayList<>();
 		ArrayList<Integer>subjectScore=new ArrayList<>();
-		String receive;
-		receive=sc.nextLine();
 		double average=0;
 		int total=0;
+
+		Scanner sc=new Scanner(System.in);
+		String receive = sc.nextLine();
 		while(receive.equals("End")==false)
 		{
 			subjectName.add(receive);
@@ -19,6 +24,8 @@ public class LessionAndScore{
 			total++;
 			receive=sc.nextLine();
 		}
+		sc.close();
+
 		average/=total;
 		for(int i=0;i<total;i++)
 			System.out.println(subjectName.get(i)+" "+subjectScore.get(i));
